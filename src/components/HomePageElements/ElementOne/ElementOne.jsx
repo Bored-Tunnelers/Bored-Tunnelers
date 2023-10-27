178eed8c2cb21d26245e7fa61c1d2ff828ff4546
talhascript <117621302+talhascript@users.noreply.github.com>
2023-10-28 00:28:16 +0800

    HOME PAGE ALMOST COMPLETE

--- src/components/HomePageElements/ElementOne/ElementOne.jsx
+++ src/components/HomePageElements/ElementOne/ElementOne.jsx
@@ -10,7 +10,7 @@ const ElementOne = () => {
       <div className="row">
         <div className="col-md-6">
           <div className="d-flex flex-column align-items-center">
-            <img src={boringLogo} alt="Boring Logo" className="img-fluid mb-1" style={{ maxWidth: '500px' }} />
+            <img src={boringLogo} alt="Boring Logo" className="img-fluid mb-1" style={{ maxWidth: '450px' }} />
             <p style={{ fontSize: '30px' }}>
               The Not-A-Boring Competition is hosted every year since 2021 by
               Elon Musk's The Boring Company. It challenges participating teams
@@ -19,8 +19,8 @@ const ElementOne = () => {
             <img src={pic2} alt="Picture 2" className="img-fluid mb-3" style={{ maxWidth: '100%' }} />
           </div>
         </div>
-        <div className="col-md-6 text-right" style={{ paddingTop: '32px' }}>
-          <div className="d-flex justify-content-end flex-column">
+        <div className="col-md-6 text-right">
+          <div className="d-flex justify-content-start flex-column align-items-start"> {/* Align elements to the left */}
             <img src={pic1} alt="Picture 1" className="img-fluid mb-3" style={{ maxWidth: '100%' }} />
             <img src={pic3} alt="Picture 3" className="img-fluid" style={{ maxWidth: '100%' }} />
           </div>
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -3,11 +3,15 @@ import { Link } from "react-router-dom";
 import videoBg from "../../assets/vid3.mp4";
 import "./homepage.css";
 import ElementOne from "../../components/HomePageElements/ElementOne/ElementOne";
+import ElementTwo from "../../components/HomePageElements/ElementTwo/ElementTwo";
+import ElementThree from "../../components/HomePageElements/ElementThree/ElementThree";
 
 const Homepage = () => {
   return (
     <>
       <ElementOne/>
+      <ElementTwo/>
+      <ElementThree/>
     </>
   );
 };
