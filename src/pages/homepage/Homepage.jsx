023040bd9217dd15aed47f8910fb4153e6ebe649
talhascript <117621302+talhascript@users.noreply.github.com>
2023-11-09 02:05:17 +0800

    SponsorPage added, Homepage video added

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -4,15 +4,19 @@ import ElementOne from "../../components/HomePageElements/ElementOne/ElementOne"
 import ElementTwo from "../../components/HomePageElements/ElementTwo/ElementTwo";
 import ElementThree from "../../components/HomePageElements/ElementThree/ElementThree";
 import TechnicalSpecifications from "../../components/HomePageElements/TechnicalSpecifications/TechnicalSpecifications";
+import videoSource from './video.mp4'; // Make sure to import the video correctly.
 
 const Homepage = () => {
   return (
-    <>
+    <div className="fullscreen-video">
+      <video autoPlay muted loop style={{ width: "100%", height: "80%" }}>
+        <source src={videoSource} type="video/mp4" />
+      </video>
       <ElementOne />
       <ElementThree />
       <ElementTwo />
       <TechnicalSpecifications />
-    </>
+    </div>
   );
 };
 
